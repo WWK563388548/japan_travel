f6bc30ad0719da3836b048bf54b890f10de99a37
WWK563388548 <wc563388548@gmail.com>
2021-06-27 13:43:57 +0800

    Adding swiper to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,11 +8,35 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  // TODO: will use local images
+  List _imageUrls = [
+    "https://i02piccdn.sogoucdn.com/549b9ec4ce259c14",
+    "https://desk-fd.zol-img.com.cn/t_s960x600c5/g2/M00/08/04/Cg-4WlUsfneIaXjtAAw3U90BfkIAABhDwJlBbUADDdr624.jpg",
+    "https://img4.cache.netease.com/photo/0006/2015-09-16/B3KICP0I5LIF0006.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home page"),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                  itemCount: _imageUrls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext context, int index){
+                    return Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
